c8871f1d725d5a968e0678de3516a7e9a372cc16
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-03-25 16:23:38 +0100

    add windowMessages

--- chat-rooms/chat-app/src/components/RoomContainer.jsx
+++ chat-rooms/chat-app/src/components/RoomContainer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const RoomContainer = ({ code, name, openRoom, setPage, setRoomData, socket, showMessage}) => {
+const RoomContainer = ({ code, name, openRoom, setPage, setRoomData, socket, setWindowMessages}) => {
     return (
         <div className="roomContainer">
             <div className="header">
                 <p className="roomName">{name}</p>
             </div>
             <p className="code">{code}</p>
-            <button className='buttonWhite' onClick={() => openRoom(code, setPage, setRoomData, socket, showMessage)}>Beitreten</button>
+            <button className='buttonWhite' onClick={() => openRoom(code, setPage, setRoomData, socket, setWindowMessages)}>Beitreten</button>
         </div>
     );
 };
