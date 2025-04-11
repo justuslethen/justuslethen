df3d106fa5b0435d3fe13c2cd2682501a3753f01
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-04-11 18:49:23 +0200

    ADD: addButton for header

--- events/app/src/components/Header.js
+++ events/app/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Header = ({ title, backButton, data }) => {
+const Header = ({ title, backButton, addButton, addAction, data }) => {
+  const navigate = useNavigate();
 
   if (!data) {
     return (
@@ -13,6 +15,14 @@ const Header = ({ title, backButton, data }) => {
           </button>
         )}
         <h1>{title}</h1>
+        {addButton && (
+          <button className='iconButtonPrimary' onClick={() => navigate(addAction)}>
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1 8L15 8" stroke="white" stroke-width="1.5" stroke-linecap="round" />
+              <path d="M8 15V1" stroke="white" stroke-width="1.5" stroke-linecap="round" />
+            </svg>
+          </button>
+        )}
       </header>
     );
   }
