2bed43620a0c8aa16ce3d54aa449a06b1620c4e0
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-04-12 13:59:41 +0200

    UPDATE: use new IconButton component

--- events/app/src/components/Header.js
+++ events/app/src/components/Header.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import IconButton from './IconButton';
 
-const Header = ({ title, backButton, addButton, addAction, data }) => {
+const Header = (props) => {
   const navigate = useNavigate();
 
-  if (!data) {
+
+  if (!props.data) {
     return (
       <header>
-        {backButton && (
-          <button className='icon-button-secondary' onClick={() => window.history.back()}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-right" style={{ transform: 'rotate(180deg)' }}>
-              <polyline points="9 18 15 12 9 6"></polyline>
-            </svg>
-          </button>
+        {props.backButton && (
+          <IconButton onclick={() => { window.history.back() }} type="secondary" icon="arrow" />
         )}
-        <h1>{title}</h1>
-        {addButton && (
-          <button className='icon-button-primary' onClick={() => navigate(addAction)}>
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1 8L15 8" stroke="white" stroke-width="1.5" stroke-linecap="round" />
-              <path d="M8 15V1" stroke="white" stroke-width="1.5" stroke-linecap="round" />
-            </svg>
-          </button>
+        <h1>{props.title}</h1>
+        {props.addButton && (
+          <IconButton onclick={() => { navigate(props.addAction) }} type="primary" icon="plus" />
         )}
       </header>
     );
