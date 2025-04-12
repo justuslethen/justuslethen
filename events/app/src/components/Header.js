0f0f5cf85b8094d099fc69e15ed474d7eb807001
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-04-12 11:29:43 +0200

    UPDATE: changed css syntax to kebab-case

--- events/app/src/components/Header.js
+++ events/app/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = ({ title, backButton, addButton, addAction, data }) => {
     return (
       <header>
         {backButton && (
-          <button className='iconButtomSecondary' onClick={() => window.history.back()}>
+          <button className='icon-button-secondary' onClick={() => window.history.back()}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-right" style={{ transform: 'rotate(180deg)' }}>
               <polyline points="9 18 15 12 9 6"></polyline>
             </svg>
@@ -16,7 +16,7 @@ const Header = ({ title, backButton, addButton, addAction, data }) => {
         )}
         <h1>{title}</h1>
         {addButton && (
-          <button className='iconButtonPrimary' onClick={() => navigate(addAction)}>
+          <button className='icon-button-primary' onClick={() => navigate(addAction)}>
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M1 8L15 8" stroke="white" stroke-width="1.5" stroke-linecap="round" />
               <path d="M8 15V1" stroke="white" stroke-width="1.5" stroke-linecap="round" />
