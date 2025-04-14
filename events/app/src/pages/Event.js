9a5ab5f516c7f314977385f1ddf1226b7dc9bc99
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-04-14 16:50:35 +0200

    ADD: get eventdata from API

--- events/app/src/pages/Event.js
+++ events/app/src/pages/Event.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
-import ListElement from '../components/ListElement';
+import WindowContainer from '../components/WindowContainer';
 
 const Event = () => {
     document.title = "Events - Event 1";
 
     const { eventId } = useParams();  // get eventId from the URL
     const addPage = `/edit/${eventId}`;  // construct the addPage URL
+    const [eventData, setEventData] = React.useState({});
+    const [pageData, setPageData] = React.useState({
+        pinInputWindow: false,
+        addSubeventEventWindow: false
+    });
+    const domain = "http://127.0.0.1:4000";
+
+    // fetch event_data from api
+    useEffect(() => {
+        fetch(`${domain}/data/get/main-event/${eventId}`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.error == "no permission") {
+                    setPageData(prev => ({ ...prev, pinInputWindow: true }))
+                }
+                setEventData(data); // set events to fetched data
+            })
+            .catch(console.error);
+    }, []);
+
 
     return (
         <>
-            <Header title="Event 1" backButton={true} addButton={true} addAction={addPage}/>
+            {pageData.pinInputWindow ? (
+                <WindowContainer title={"PIN für " + eventData.name + " benötigt"} innerContent=''/>
+            ) : pageData.addSubeventEventWindow ? (
+                <WindowContainer title={"Neuen Programmpunkt für " + eventData.name + " erstellen"} innerContent='' />
+            ) : null}
+
+            <Header title="Event 1" backButton={true} addButton={true} addAction={addPage} />
+            {console.log(eventData)}
         </>
     );
 };
