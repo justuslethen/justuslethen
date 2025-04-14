45794bffdbb57254c9bcea2e0ce137151260eeb8
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-04-14 21:35:32 +0200

    UPDATE: now create token if doesnt exist

--- events/modules/permission.py
+++ events/modules/permission.py
@@ -25,7 +25,7 @@ def create_random_token(cur):
     while token_exists:
         # create random 20 char string
         for i in range(20):
-            token += random.choise(string)
+            token += random.choice(string)
             
         token_exists = get_token(cur, token)
         
@@ -34,41 +34,54 @@ def create_random_token(cur):
 
 def get_token(cur, token):
     # select any if token exists
-    cur.execute("SELECT * FROM tokens WHERE token = ?", token)
+    cur.execute("SELECT token_id FROM tokens WHERE token = ?", (token,))
     res = cur.fetchone()
-    return res # return tuple
+    return res[0] if res else False # return token_id
 
 
 def create_token_row(cur, token):
-    cur.execute("INSERT INTO tokens (token) VALUES (?)", (token))
+    cur.execute("INSERT INTO tokens (token) VALUES (?)", (token,))
     
     return cur.lastrowid # return token_id
 
 
-def check_room_permissions(event_id, pin):
-    token = request.cookies.get("token")
+def check_event_permissions(event_id, pin):
+    token = request.cookies.get("token") # get token from cockie
+    token_existed = True
     
     cur, conn = database.load()
-    has_access = False
     
+    # check if token exists in db
+    if not get_token_id(cur, token):
+        token = build_new_token()
+        
+        # return False for the function that has called to set a new cockie as res
+        token_existed = False
+    
+    has_access = check_access(cur, token, event_id, pin)
+    
+    conn.commit()
+    conn.close()
+    
+    return has_access, token_existed, token
+
+
+def check_access(cur, token, event_id, pin):
     # check if user already has permissions
     if has_token_access_to_event(cur, token, event_id):
-        has_access =  True
+        return True
     
     # if entered pin is coreect give permissions
     elif check_pin(cur, event_id, pin):
         add_event_to_permissions(cur, token, event_id) # create new permissions row
-        has_access = True
+        return True
         
     # if pin is empty give permissions
     elif not has_event_a_pin(cur, event_id):
         add_event_to_permissions(cur, token, event_id) # create new permissions row
-        has_access = True
-        
-    conn.commit()
-    conn.close()
+        return True
     
-    return has_access
+    return False
 
 
 def has_event_a_pin(cur, event_id):
