a5381b6b7a53b3cc4d10e1339ed518787dd187ac
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-04-15 12:31:59 +0200

    UPDATE: use IconButton

--- events/app/src/components/ListElement.js
+++ events/app/src/components/ListElement.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import IconButton from './IconButton'
 
 const ListElement = ({ name, startDate, endDate, onClick }) => {
   const formatDate = (isoDate) => {
@@ -20,11 +21,7 @@ const ListElement = ({ name, startDate, endDate, onClick }) => {
           <p>{formatDate(startDate)} - {formatDate(endDate)}</p>
         </div>
       </div>
-      <button className='icon-button-secondary'>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-right">
-          <polyline points="9 18 15 12 9 6"></polyline>
-        </svg>
-      </button>
+      <IconButton onclick={() => { }} type="secondary" icon="arrow" />
     </div>
   );
 };
