6b6296f6aa0e37838a4e929af8492d34ee44f106
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-04-15 23:33:59 +0200

    ADD: focus event

--- events/app/src/components/SubEventsTimeline.js
+++ events/app/src/components/SubEventsTimeline.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SubEventContainer from './SubEventContainer'
 
 const SubEventsTimeline = (props) => {
+    const [subevents, setSubevents] = useState(props.subevents);
+
+    const changeFocused = (index) => {
+        // set the event that was clicked on on the other focused state than it is
+        const updated = [...subevents];
+        updated[index] = {
+            ...updated[index],
+            focused: !updated[index].focused,
+        };
+        setSubevents(updated); // save updated version
+    };
+
     return (
         <div className='section-container'>
             <div className='timeline-stroke'></div>
             <div className='event-list'>
-                {console.log(props.subevents)}
-                {props.subevents.map((subevent, index) => (
-                    <SubEventContainer key={index} subevent={subevent} />
+                {subevents.map((subevent, index) => (
+                    <SubEventContainer key={index} subevent={subevent} onclick={() => { changeFocused(index) }} />
                 ))}
             </div>
         </div>
