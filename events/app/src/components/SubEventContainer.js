21947296da13af384f5ff3aa68cc7585ae2c180b
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-04-15 23:33:47 +0200

    ADD: rows rendereing and eventRunnign and eventOver

--- events/app/src/components/SubEventContainer.js
+++ events/app/src/components/SubEventContainer.js
@@ -3,15 +3,42 @@ import IconButton from './IconButton';
 
 const SubEventContainer = (props) => {
     // make from yyyy-mm-ddThh:mm to hh:mm
-    const startTime = new Date(props.subevent.startdate).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }).replace(':', '.');
+    const startTime = new Date(props.subevent.startdate).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+    
+    // get all date data
+    const now = new Date();
+    const startDate = new Date(props.subevent.startdate);
+    const endDate = new Date(props.subevent.enddate);
+
+    // calc if event is running or over
+    const isEventRunning = now >= startDate && now <= endDate;
+    const isEventOver = now > endDate;
 
     return (
-        <div className='subevent-container'>
+        <div
+            className={
+                "subevent-container" +
+                (isEventRunning ? " event-running" : isEventOver ? " event-over" : "")
+            }
+            onClick={props.onclick}
+        >
             <div className='header'>
-                <p className='event-time'>{startTime}</p>
+                <p className='event-time'>{startTime}Uhr</p>
                 <p className='event-name'>{props.subevent.subeventname}</p>
-                <IconButton type='secondary' icon='arrow' rotate={0.75}/>
+                {props.subevent.rows.length > 0 ? (
+                    <IconButton type='secondary' icon='arrow' rotate={props.subevent.focused ? 0.25 : 0.75} />
+                ) : (null)}
             </div>
+            {props.subevent.rows.length > 0 && props.subevent.focused ? (
+                <div className='table'>
+                    {props.subevent.rows.map((row, index) => (
+                        <div className='row' key={index}>
+                            <div className='col'>{row.name}</div>
+                            <div className='col'>{row.context}</div>
+                        </div>
+                    ))}
+                </div>
+            ) : (null)}
         </div>
     )
 }
