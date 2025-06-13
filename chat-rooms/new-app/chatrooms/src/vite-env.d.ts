03b77d25e1becb0eb432506ea4abbf47aec33793
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-13 20:39:41 +0200

    ADD: svg and css module

--- chat-rooms/new-app/chatrooms/src/vite-env.d.ts
+++ chat-rooms/new-app/chatrooms/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+
+declare module '*.svg' {
+  import * as React from 'react';
+  const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default content;
+}
