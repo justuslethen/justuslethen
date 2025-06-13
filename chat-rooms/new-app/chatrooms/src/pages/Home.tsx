c3ae1e6b297bd15b63cad1720043f99433a68a6b
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-13 20:42:35 +0200

    ADD: language support

--- chat-rooms/new-app/chatrooms/src/pages/Home.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/Home.tsx
@@ -1,6 +1,12 @@
+// import { t } from '../i18n';
+import Text from '../components/Text/Text.tsx';
+
 const Home = () => {
     return (
         <>
+            <div className='content'>
+                <Text text="Herzlich wilkommen" type="h1" />
+            </div>
         </>
     )
 }
