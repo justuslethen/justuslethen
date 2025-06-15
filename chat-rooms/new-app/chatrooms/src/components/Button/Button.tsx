5b380211ee1a50e500cbfa2d903bc55f443c372c
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-15 17:41:30 +0200

    ADD: button oclick prop

--- chat-rooms/new-app/chatrooms/src/components/Button/Button.tsx
+++ chat-rooms/new-app/chatrooms/src/components/Button/Button.tsx
@@ -3,11 +3,12 @@ import styles from './Button.module.css';
 interface ButtonProps {
     text: string
     color: string
+    onclick: () => void
 }
 
 const Button = (props: ButtonProps) => {
     return (
-        <button className={`${styles.button} ${styles["button-" + props.color]}`}>
+        <button className={`${styles.button} ${styles["button-" + props.color]}`} onClick={props.onclick}>
             {props.text}
         </button>
     )
--- chat-rooms/new-app/chatrooms/src/pages/Home/Home.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/Home/Home.tsx
@@ -4,18 +4,21 @@ import Spaceholder from '../../components/Spaceholder/Spaceholder.tsx';
 import Button from '../../components/Button/Button.tsx';
 import Background from '../../components/Background/Background.tsx';
 import styles from './Home.module.css';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
+
     return (
         <>
             <Background design={1} />
             <div className={`content ${styles.content}`}>
                 <section className={styles.section}>
-                    <Text text={t("welcome")} type="h1" center={true} />
-                    <Text text={t("general_description")} type="description" center={true} />
+                    <Text text={t("home.welcome")} type="h1" center={true} />
+                    <Text text={t("home.description")} type="description" center={true} />
                     <Spaceholder size={3} />
-                    <Button text={t("links.dicover_rooms")} color="grey" />
-                    <Button text={t("links.create_room")} color="black" />
+                    <Button text={t("links.dicover_rooms")} color="grey" onclick={() => { navigate("/public-rooms") }} />
+                    <Button text={t("links.create_room")} color="black" onclick={() => { navigate("/create-room") }}  />
                 </section>
             </div>
         </>
