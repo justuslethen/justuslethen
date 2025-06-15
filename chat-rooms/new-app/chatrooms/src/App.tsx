8c280f4623f1314430c41a3759597afd5316fe7a
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-15 18:39:14 +0200

    ADD: PublicRooms page

--- chat-rooms/new-app/chatrooms/src/App.tsx
+++ chat-rooms/new-app/chatrooms/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.tsx';
 import NotFound from './pages/NotFound/NotFound.tsx';
+import PublicRooms from './pages/PublicRooms/PublicRooms.tsx';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/public-rooms" element={<PublicRooms />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
