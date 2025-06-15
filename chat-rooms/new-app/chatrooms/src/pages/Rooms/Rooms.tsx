4a17c2a704aa05e0f66ad8a6351ba36cc3db3884
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-15 21:31:43 +0200

    UPDATE: main page with subpage

---
+++ chat-rooms/new-app/chatrooms/src/pages/Rooms/Rooms.tsx
@@ -0,0 +1,25 @@
+import { t } from '../../i18n.ts';
+import Text from '../../components/Text/Text.tsx';
+import Navbar from '../../components/Navbar/Navbar.tsx';
+import styles from './Rooms.module.css';
+import { Outlet } from 'react-router-dom';
+
+const Rooms = () => {
+    return (
+        <>
+            <div className={`content ${styles.content1}`}>
+                <Text text={t("rooms.title")} type="h1" />
+            </div>
+
+            <Navbar links={[{ path: "public", tag: t("rooms.public") },
+            { path: "recent", tag: t("rooms.recent") }
+            ]} />
+
+            <div className={`content ${styles.content}`}>
+                <Outlet />
+            </div>
+        </>
+    )
+}
+
+export default Rooms;
