0d0827e85bfcfd884c2a789996c976094823eefa
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-15 16:35:44 +0200

    ADD: backgrounds

--- chat-rooms/new-app/chatrooms/src/pages/Home.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/Home.tsx
@@ -1,11 +1,19 @@
-// import { t } from '../i18n';
+import { t } from '../i18n';
 import Text from '../components/Text/Text.tsx';
+import Spaceholder from '../components/Spaceholder/Spaceholder.tsx';
+import Button from '../components/Button/Button.tsx';
+import Background from '../components/Background/Background.tsx';
 
 const Home = () => {
     return (
         <>
+        <Background design={1} />
             <div className='content'>
-                <Text text="Herzlich wilkommen" type="h1" />
+                <Text text={t("welcome")} type="h1" />
+                <Text text={t("general_description")} type="description" />
+                <Spaceholder size={3} />
+                <Button text={t("links.dicover_rooms")} color="white"/>
+                <Button text={t("links.create_room")} color="black"/>
             </div>
         </>
     )
