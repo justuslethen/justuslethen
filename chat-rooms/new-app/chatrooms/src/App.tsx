c59d78087e132d7198c3497c553b99421b9d6d2a
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-15 21:30:48 +0200

    ADD: new paths

--- chat-rooms/new-app/chatrooms/src/App.tsx
+++ chat-rooms/new-app/chatrooms/src/App.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.tsx';
 import NotFound from './pages/NotFound/NotFound.tsx';
-import PublicRooms from './pages/PublicRooms/PublicRooms.tsx';
+import Rooms from './pages/Rooms/Rooms.tsx';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/public-rooms" element={<PublicRooms />} />
+      <Route path="/rooms" element={<Rooms />}>
+        <Route path="public" element={<p>Public</p>} />
+        <Route path="recent" element={<p>Recent</p>} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
