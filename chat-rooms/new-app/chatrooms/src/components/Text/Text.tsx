37cb62c00c84b80708ae0da573611a1c22b25ecb
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-15 16:36:22 +0200

    Update: text module

--- chat-rooms/new-app/chatrooms/src/components/Text/Text.tsx
+++ chat-rooms/new-app/chatrooms/src/components/Text/Text.tsx
@@ -3,12 +3,17 @@ import styles from './Text.module.css';
 interface TextProps {
     text: string
     type: string
+    color?: number
 }
 
 const Text = (props: TextProps) => {
     return (
         <div className={`${styles.container} ${styles["container-" + props.type]}`}>
-            <p className={`${styles.text} ${styles["text-" + props.type]}`}>
+            <p className={`
+                ${styles.text}
+                ${styles["text-" + props.type]}
+                ${styles["color-" + props.color]}
+            `}>
                 {props.text}
             </p>
         </div>
