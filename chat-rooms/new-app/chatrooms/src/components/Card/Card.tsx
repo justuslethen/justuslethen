8778197561913c4bacb548d9fe9f528dd3036390
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-16 15:27:56 +0200

    ADD: card module

---
+++ chat-rooms/new-app/chatrooms/src/components/Card/Card.tsx
@@ -0,0 +1,15 @@
+import styles from './Card.module.css';
+
+interface CardProps {
+    children: React.ReactNode
+}
+
+const Card = (props: CardProps) => {
+    return (
+        <div className={`${styles.card}`}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Card;
