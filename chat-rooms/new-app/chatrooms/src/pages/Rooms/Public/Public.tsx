de7518d2992b83413bb5fc39323901a91186eab0
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-16 19:09:14 +0200

    ADD: render function to PubicRooms

--- chat-rooms/new-app/chatrooms/src/pages/Rooms/Public/Public.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/Rooms/Public/Public.tsx
@@ -3,14 +3,29 @@ import Text from '../../../components/Text/Text.tsx';
 import Card from '../../../components/Card/Card.tsx'
 import RoomCard from '../../../components/RoomCard/RoomCard.tsx'
 import styles from './Public.module.css';
+import { useState, useEffect } from 'react';
 
 const Public = () => {
+    const [rooms, setRooms] = useState([
+        {
+            name: "Room",
+            "code": "KHDIEK",
+            "online": 4
+        }]);
+
+
     return (
         <>
             <Text text={t("rooms.public.title")} type="h2" />
-            <Card>
-                <RoomCard online={1} code="JHGDIE" name="Room" />
-            </Card>
+            {rooms.map(room => {
+                return (
+                    <>
+                        <Card>
+                            <RoomCard online={room.online} code={room.code} name={room.name} />
+                        </Card>
+                    </>
+                )
+            })}
         </>
     )
 }
