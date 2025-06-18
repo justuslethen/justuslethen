a18296c77d540baa38e2311c238084c5ba42ae02
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-18 19:17:00 +0200

    ADD: nav button to room page

--- chat-rooms/new-app/chatrooms/src/pages/Rooms/Public/Public.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/Rooms/Public/Public.tsx
@@ -3,7 +3,6 @@ import Text from '../../../components/Text/Text.tsx';
 import Card from '../../../components/Card/Card.tsx'
 import RoomCard from '../../../components/RoomCard/RoomCard.tsx'
 import styles from './Public.module.css';
-import BottomBar from '../../../components/BottomBar/BottomBar.tsx';
 import { useState, useEffect } from 'react';
 
 const Public = () => {
@@ -17,10 +16,6 @@ const Public = () => {
 
     return (
         <>
-
-            <BottomBar>
-
-            </BottomBar>
             <Text text={t("rooms.public.title")} type="h2" />
             {rooms.map(room => {
                 return (
--- chat-rooms/new-app/chatrooms/src/pages/Rooms/Rooms.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/Rooms/Rooms.tsx
@@ -2,11 +2,19 @@ import { t } from '../../i18n.ts';
 import Text from '../../components/Text/Text.tsx';
 import Navbar from '../../components/Navbar/Navbar.tsx';
 import styles from './Rooms.module.css';
+import { useNavigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
+import BottomBar from '../../components/BottomBar/BottomBar.tsx';
+import Button from '../../components/Button/Button.tsx';
 
 const Rooms = () => {
+    const navigate = useNavigate();
     return (
         <>
+            <BottomBar>
+                <Button text={t("rooms.create_room_btn")} color="black" onclick={() => { navigate("/create-room") }} />
+            </BottomBar>
+
             <div className="text-content">
                 <Text text={t("rooms.title")} type="h1" />
             </div>
