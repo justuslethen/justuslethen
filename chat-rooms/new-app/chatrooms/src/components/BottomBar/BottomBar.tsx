84398dadd89807e599f45f17a6e453b5708048d0
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-18 16:30:39 +0200

    ADD: BottomBar component

---
+++ chat-rooms/new-app/chatrooms/src/components/BottomBar/BottomBar.tsx
@@ -0,0 +1,13 @@
+import styles from './BottomBar.module.css';
+
+const BottomBar = () => {
+    return (
+        <>
+        <div className={styles.bottomBar}>
+
+        </div>
+        </>
+    )
+}
+
+export default BottomBar;
--- chat-rooms/new-app/chatrooms/src/pages/Rooms/Public/Public.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/Rooms/Public/Public.tsx
@@ -3,6 +3,7 @@ import Text from '../../../components/Text/Text.tsx';
 import Card from '../../../components/Card/Card.tsx'
 import RoomCard from '../../../components/RoomCard/RoomCard.tsx'
 import styles from './Public.module.css';
+import BottomBar from '../../../components/BottomBar/BottomBar.tsx';
 import { useState, useEffect } from 'react';
 
 const Public = () => {
@@ -16,6 +17,10 @@ const Public = () => {
 
     return (
         <>
+
+            <BottomBar>
+
+            </BottomBar>
             <Text text={t("rooms.public.title")} type="h2" />
             {rooms.map(room => {
                 return (
