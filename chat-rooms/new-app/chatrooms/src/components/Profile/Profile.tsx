45198e268a61a681129e026d18ec78e95dc65f98
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-19 16:45:21 +0200

    ADD: profile component

---
+++ chat-rooms/new-app/chatrooms/src/components/Profile/Profile.tsx
@@ -0,0 +1,24 @@
+import styles from './Profile.module.css';
+import ColorSelect from '../ColorSelect/ColorSelect.tsx';
+import { t } from '../../i18n.ts';
+
+const Profile = () => {
+    const profile = {
+        name: "Justus",
+        color: "purple"
+    }
+
+    return (
+        <>
+            <div className={styles.profileContainer}>
+                <div className={styles.name}>
+                    <p className={styles.label}>{t("join_room.profile.name.title")}</p>
+                    <input className={styles.input} placeholder={t("join_room.profile.name.placeholder")}></input>
+                </div>
+                <ColorSelect />
+            </div>
+        </>
+    )
+}
+
+export default Profile;
