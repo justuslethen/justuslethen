461be57cea8633a922da598c3dd36187f8c0d81b
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-19 09:03:41 +0200

    ADD: comments to selection component

--- chat-rooms/new-app/chatrooms/src/components/Selection/Selection.tsx
+++ chat-rooms/new-app/chatrooms/src/components/Selection/Selection.tsx
@@ -15,14 +15,26 @@ const Selection = (props: SelectionProps) => {
     const selectRef = useRef<HTMLSelectElement>(null);
     const [selectedValue, changeSelectedValue] = useState(props.options[0].value);
 
+    /**
+     * change the selected element in the UI
+     * change to the value of <select>
+     */
     const changeSelected = () => {
         if (selectRef.current) {
             const value = selectRef.current.value;
             changeSelectedValue(value);
         }
+
+        // wait till object is unfocused and focus again
         setTimeout(focusSelect, 500);
     }
 
+    /**
+     * call this funcion when the UI selection is clicked
+     * change the selected value to a specific value
+     * value given in function
+     * update <select>
+     */
     const changeSelectedTo = (value: string) => {
         if (selectRef.current) {
             selectRef.current.value = value;
@@ -31,7 +43,11 @@ const Selection = (props: SelectionProps) => {
         focusSelect();
     }
 
+    /**
+     * make the <select> focused for autocomplete
+     */
     const focusSelect = () => {
+        // only focus if exists
         if (selectRef.current) {
             selectRef.current.focus();
         }
@@ -48,8 +64,11 @@ const Selection = (props: SelectionProps) => {
                     ${props.vertical ? styles["options-vertical"] : null}
                 `}>
                     {props.options.map((option, index) => {
+                        // render the options in the array as divs
+                        // give -selected class if is selected
+
                         return (
-                            <div className={`
+                            <div key={index} className={`
                                 ${styles.option}
                                 ${selectedValue == option.value ? styles["option-selected"] : null}
                             `}
@@ -59,8 +78,14 @@ const Selection = (props: SelectionProps) => {
                     })}
                 </div>
             </div>
+
+            {/* render hidden select */}
             <select ref={selectRef} onChange={changeSelected} className={styles.hiddenSelect}>
                 {props.options.map((option, index) => {
+                    // render the options in the array as options
+                    // provide functionallity with the select object
+                    // hide <select>
+
                     return (
                         <option key={index} value={option.value}>
                             {option.label}
