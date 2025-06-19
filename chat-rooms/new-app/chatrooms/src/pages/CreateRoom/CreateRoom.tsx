a0341616e06f202d61f70b9dc86bc4da1851df8e
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-19 09:30:03 +0200

    ADD: select roomDuration to creatRoom

--- chat-rooms/new-app/chatrooms/src/pages/CreateRoom/CreateRoom.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/CreateRoom/CreateRoom.tsx
@@ -11,10 +11,19 @@ const CreateRoom = () => {
         { value: "private", label: t("create_room.options.private") },
         { value: "public", label: t("create_room.options.public") }];
 
-    const roomEndOptions = [
+    const maxMembersOptions = [
         { value: "5", label: "5" },
         { value: "20", label: "20" },
+        { value: "50", label: "50" },
         { value: "100", label: "100" }];
+
+    const roomDurationOprions = [
+        {value: "0", label: "0"},
+        {value: "1", label: "1"},
+        {value: "3", label: "3"},
+        {value: "7", label: "7"},
+        {value: "30", label: "30"},
+    ]
 
     return (
         <>
@@ -34,13 +43,18 @@ const CreateRoom = () => {
 
                 <Selection options={accessibilityOptions} title={t("create_room.options.header.accessibility")} />
                 <Text text={t("create_room.select_description_1")} type="description" />
-                <Spaceholder size={5} />
+                <Spaceholder size={3} />
 
-                <Selection options={roomEndOptions} title={t("create_room.options.header.max_members")} />
+                <Selection options={maxMembersOptions} title={t("create_room.options.header.max_members")} />
                 <Text text={t("create_room.select_description_2")} type="description" />
+                <Spaceholder size={3} />
+
+                <Selection options={roomDurationOprions} title={t("create_room.options.header.room_duraction")} />
+                <Text text={t("create_room.select_description_3")} type="description" />
                 <Spaceholder size={5} />
 
-                <Button text={t("create_room.create_btn")} color="orange" onclick={() => { }} />
+                <Button text={t("create_room.create_btn")} color="green" onclick={() => { }} />
+                <Spaceholder size={5} />
             </div>
         </>
     )
