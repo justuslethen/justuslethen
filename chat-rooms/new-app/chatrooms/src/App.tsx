aff769538d0ca85fe888f997cc0327b4ae927588
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-19 10:09:41 +0200

    ADD: joinRoom page

--- chat-rooms/new-app/chatrooms/src/App.tsx
+++ chat-rooms/new-app/chatrooms/src/App.tsx
@@ -5,16 +5,22 @@ import Rooms from './pages/Rooms/Rooms.tsx';
 import Recent from './pages/Rooms/Recent/Recent.tsx';
 import Public from './pages/Rooms/Public/Public.tsx';
 import CreateRoom from './pages/CreateRoom/CreateRoom.tsx';
+import JoinRoom from './pages/JoinRoom/JoinRoom.tsx';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
       <Route path="/create-room" element={<CreateRoom />} />
+
+      <Route path="/join-room/*" element={<JoinRoom />} />
+
       <Route path="/rooms" element={<Rooms />}>
         <Route path="public" element={<Public />} />
         <Route path="recent" element={<Recent />} />
       </Route>
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
