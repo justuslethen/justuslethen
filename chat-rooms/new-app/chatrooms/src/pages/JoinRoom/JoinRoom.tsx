ab69689a7b5709c8be69fbf9475190815c92ec8f
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-19 16:45:42 +0200

    UPDATE: show profile in joinRoom

--- chat-rooms/new-app/chatrooms/src/pages/JoinRoom/JoinRoom.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/JoinRoom/JoinRoom.tsx
@@ -1,8 +1,18 @@
 import styles from './JoinRoom.module.css';
+import Text from '../../components/Text/Text.tsx';
+import Profile from '../../components/Profile/Profile.tsx';
+import { t } from '../../i18n.ts';
 
 const JoinRoom = () => {
     return (
         <>
+            <div className='text-content'>
+                <Text text={t("app_name")} type="h1" />
+                <Text text={t("join_room.title")} type="h2" />
+            </div>
+            <div className='content'>
+                <Profile />
+            </div>
         </>
     )
 }
