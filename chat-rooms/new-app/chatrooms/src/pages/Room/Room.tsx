667f0ede4199d6685047291118009481a9910eda
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-21 16:50:41 +0200

    ADD: navbar links

--- chat-rooms/new-app/chatrooms/src/pages/Room/Room.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/Room/Room.tsx
@@ -1,6 +1,25 @@
+import Navbar from '../../components/Navbar/Navbar.tsx';
+import Text from '../../components/Text/Text.tsx';
+import { t } from '../../i18n.ts';
+import { Outlet } from 'react-router-dom';
+
 const Room = () => {
+    const links = [
+        { path: "info", tag: t("room.link.info") },
+        { path: "users", tag: t("room.link.user") },
+        { path: "profile", tag: t("room.link.profile") }
+    ];
+
     return (
         <>
+            <div className="text-content">
+                <Text text={t("app_name")} type="h1" />
+            </div>
+            <Navbar links={links} />
+
+            <div className="content">
+                <Outlet />
+            </div>
         </>
     )
 }
