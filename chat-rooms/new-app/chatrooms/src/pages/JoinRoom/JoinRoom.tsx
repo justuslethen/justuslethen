be8b2f01723c0f125d5694629bc5f5a803e420ac
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-21 16:23:35 +0200

    ADD: UI fro JoinRoom

--- chat-rooms/new-app/chatrooms/src/pages/JoinRoom/JoinRoom.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/JoinRoom/JoinRoom.tsx
@@ -1,6 +1,8 @@
 import styles from './JoinRoom.module.css';
 import Text from '../../components/Text/Text.tsx';
 import Profile from '../../components/Profile/Profile.tsx';
+import Button from '../../components/Button/Button.tsx';
+import Spaceholder from '../../components/Spaceholder/Spaceholder.tsx';
 import { t } from '../../i18n.ts';
 
 const JoinRoom = () => {
@@ -11,7 +13,11 @@ const JoinRoom = () => {
                 <Text text={t("join_room.title")} type="h2" />
             </div>
             <div className='content'>
+                <Text text={t("join_room.title_2")} type="h3" />
                 <Profile />
+                <Text text={t("join_room.description_1")} type="description" />
+                <Spaceholder size={3} />
+                <Button text={t("join_room.join_btn")} color="black" onclick={() => { }} />
             </div>
         </>
     )
