63a416c6f6e598818bbb34f3204273efa4693c98
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-22 09:06:41 +0200

    build room/info page

--- chat-rooms/new-app/chatrooms/src/pages/Room/RoomUserProfile/RoomUserProfile.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/Room/RoomUserProfile/RoomUserProfile.tsx
@@ -1,6 +1,17 @@
+import Text from '../../../components/Text/Text.tsx';
+import Profile from '../../../components/Profile/Profile.tsx';
+import Button from '../../../components/Button/Button.tsx';
+import Spaceholder from '../../../components/Spaceholder/Spaceholder.tsx';
+import { t } from '../../../i18n.ts';
+
 const RoomUserProfile = () => {
     return (
         <>
+            <Text text={t("room.profile.title")} type="h2" />
+            <Profile />
+            <Spaceholder size={3} />
+            <Button text={t("room.profile.save_changes_btn")} color="black" onclick={() => { }} />
+            <Button text={t("room.profile.leave_btn")} color="grey" onclick={() => { }} />
         </>
     )
 }
