51b3b7b0a721525b433458a7f44700d0ebb17f21
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-06-22 22:22:48 +0200

    make use of table component instead of text

--- chat-rooms/new-app/chatrooms/src/pages/Room/RoomInfo/RoomInfo.tsx
+++ chat-rooms/new-app/chatrooms/src/pages/Room/RoomInfo/RoomInfo.tsx
@@ -1,6 +1,36 @@
+import Text from '../../../components/Text/Text.tsx';
+import Table from '../../../components/Table/Table.tsx';
+import Spaceholder from '../../../components/Spaceholder/Spaceholder.tsx';
+import { t } from '../../../i18n.ts';
+
+
 const RoomInfo = () => {
+    const tableData_1 = [
+        ["Members online:", "10 / 20 (50%)"],
+        ["Duration alive:", "4h / 1d (17%)"],
+        ["Duration alive:", "4h / 1d (17%)"],
+        ["Accessability:", "private"]
+    ];
+
+    const tableData_2 = [
+        ["Messages sent:", "213"],
+        ["Number of users over lifetime:", "27"],
+        ["Users kicked:", "2"],
+        ["Users timeouted:", "1"]
+    ];
+
+
     return (
         <>
+            <Text text={t("room.info.title")} type="h2" />
+            <Spaceholder size={3} />
+
+            <Text text={t("room.info.specs.title")} type="h3" />
+            <Table table={tableData_1} />
+            <Spaceholder size={3} />
+
+            <Text text={t("room.info.data.title")} type="h3" />
+            <Table table={tableData_2} />
         </>
     )
 }
