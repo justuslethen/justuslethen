bfca0072a40215203c9d503337fc5bc3b10b837d
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-08-20 17:55:00 +0200

    add token and loginAttempts table

--- login/go-backend/internal/database.go
+++ login/go-backend/internal/database.go
@@ -11,7 +11,6 @@ import (
 
 var DB *sql.DB
 
-
 func Connect() error {
 	// get server config with db config
 	config := config.ServerConfig
@@ -36,22 +35,57 @@ func Connect() error {
 	return nil
 }
 
-
 func setupDatabase() {
 	createUserTable()
+	createTokensTable()
+	createLoginAttempts()
 }
-
 
 func createUserTable() {
 	_, err := DB.Exec(`
     CREATE TABLE IF NOT EXISTS users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
+        userid INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
         username VARCHAR(100) NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
 		ip_created VARCHAR(20) NOT NULL,
+		password VARCHAR(40) NOT NULL,
         last_used TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+	`)
+	if err != nil {
+		log.Fatal("Failed creating table:", err)
+	}
+}
+
+func createTokensTable() {
+	_, err := DB.Exec(`
+    CREATE TABLE IF NOT EXISTS tokens (
+        tokenid INT AUTO_INCREMENT PRIMARY KEY,
+        userid INT AUTO_INCREMENT PRIMARY KEY,
+        token VARCHAR(100) NOT NULL,
+        username VARCHAR(100) NOT NULL,
+        ip_last_used VARCHAR(20) UNIQUE NOT NULL,
+		ip_created VARCHAR(20) NOT NULL,
+		number_used VARCHAR(9) NOT NULL,
+        last_used TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+	`)
+	if err != nil {
+		log.Fatal("Failed creating table:", err)
+	}
+}
+
+func createLoginAttempts() {
+	_, err := DB.Exec(`
+    CREATE TABLE IF NOT EXISTS login_attempts (
+        loginid INT AUTO_INCREMENT PRIMARY KEY,
+		ip VARCHAR(20) NOT NULL,
+		userid VARCHAR(20) NOT NULL,
+		number_used VARCHAR(9) NOT NULL,
+        tried_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
 	`)
 	if err != nil {
