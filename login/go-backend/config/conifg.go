af2be1236ab13839ca38fc9bbd9fcf75bca0072b
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-08-20 14:17:25 +0200

    init serverconfig

---
+++ login/go-backend/config/conifg.go
@@ -0,0 +1,15 @@
+// config/config.go
+package config
+
+import (
+    // "log"
+    // "os"
+
+    // "github.com/joho/godotenv"
+)
+
+type Config struct {
+    DBUser     string
+    DBPassword string
+    Port       string
+}
