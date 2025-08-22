394d1bc2fa7ca628360f3f406c32cf89d059289e
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-08-22 15:22:00 +0200

    add login page

---
+++ login/login-app/src/pages/Login/Login.tsx
@@ -0,0 +1,20 @@
+// import { t } from '../../i18n.ts';
+// import Text from '../../components/Text/Text.tsx';
+// import Spaceholder from '../../components/Spaceholder/Spaceholder.tsx';
+// import Button from '../../components/Button/Button.tsx';
+import styles from './Home.module.css';
+// import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+    // const navigate = useNavigate();
+
+    return (
+        <>
+            <div className={`content ${styles.content}`}>
+                
+            </div>
+        </>
+    )
+}
+
+export default Login;
