3716f1f83ebb92667803a84fb1c3e11c339305d3
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-08-23 12:22:43 +0200

    build basic registration page layout

--- login/login-app/src/pages/Registrate/Registrate.tsx
+++ login/login-app/src/pages/Registrate/Registrate.tsx
@@ -1,8 +1,9 @@
 import { t } from '../../i18n.ts';
 import Text from '../../components/Text/Text.tsx';
 // import Spaceholder from '../../components/Spaceholder/Spaceholder.tsx';
-// import Button from '../../components/Button/Button.tsx';
-import styles from './Home.module.css';
+import Button from '../../components/Button/Button.tsx';
+import Input from '../../components/Input/Input.tsx';
+import styles from './Registrate.module.css';
 // import { useNavigate } from 'react-router-dom';
 
 const Registrate = () => {
@@ -11,11 +12,32 @@ const Registrate = () => {
     return (
         <>
             <div className="text-content">
-                <Text text={t("rooms.title")} type="h1" />
+                <Text text={t("app_name")} type="h1" />
+                <Text text={t("registrate.title")} type="h2" center={true} />
             </div>
 
             <div className={`content ${styles.content}`}>
-                
+                <div className={styles.container}>
+                    <section className={styles.section}>
+                        <Input label={t("input.email.label")} placeholder={t("input.email.placeholder")} />
+                    </section>
+
+                    <section className={styles.section}>
+                        <Input label={t("input.username.label")} placeholder={t("input.username.placeholder")} />
+                        <Text text={t("registrate.username.description")} type="description" />
+                    </section>
+
+                    <section className={styles.section}>
+                        <Input label={t("input.name.label")} placeholder={t("input.name.placeholder")} />
+                    </section>
+
+                    <section className={styles.section}>
+                        <Input label={t("input.password.label")} placeholder={t("input.password.placeholder")} />
+                        <Text text={t("registrate.password.description")} type="description" />
+                    </section>
+
+                    <Button text={t("registrate.button")} color="black" onclick={() => { /* Handle login */ }} />
+                </div>
             </div>
         </>
     )
