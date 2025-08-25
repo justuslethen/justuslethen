38a1cdd18a9ea0fc7a86b3604fb09680e04016ca
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-08-25 12:55:00 +0200

    init RegisterUser func and decode json to struct

--- login/go-backend/internal/register.go
+++ login/go-backend/internal/register.go
@@ -4,13 +4,41 @@ import (
 	"fmt"
 	"net/http"
 	// "io/ioutil"
-	// "encoding/json"
+	"encoding/json"
 	// "go-backend/internal/database"
 	// "log"
 	// "time"
 )
 
+type RegisterRequestForm struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Name     string `json:"name"`
+	Bio      string `json:"bio"`
+}
 
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("register new user", w, r)
+	// Ensure method is POST
+	if r.Method != http.MethodPost {
+		return
+	}
+
+	// Decode JSON body into a struct
+	var requestData RegisterRequestForm
+
+	err := json.NewDecoder(r.Body).Decode(&requestData)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	fmt.Printf("Received data: %+v\n", requestData)
+
+	// Example: run validations
+	// checkRegisterFormPatterns(&requestData)
+
+	// sendregisterResponse(w, &requestData)
 }
+
