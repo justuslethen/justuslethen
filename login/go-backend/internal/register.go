c427758492cb9ad20338bdfc6867a7e474bfb3cc
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-08-25 12:53:28 +0200

    init register handler

---
+++ login/go-backend/internal/register.go
@@ -0,0 +1,16 @@
+package internal
+
+import (
+	"fmt"
+	"net/http"
+	// "io/ioutil"
+	// "encoding/json"
+	// "go-backend/internal/database"
+	// "log"
+	// "time"
+)
+
+
+func RegisterUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("register new user", w, r)
+}
--- login/go-backend/server/server.go
+++ login/go-backend/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+    "fmt"
     "net/http"
     "go-backend/internal"
     // "go-backend/pkg"
@@ -9,13 +10,17 @@ import (
 func Start(addr string) error {
     mux := http.NewServeMux()
 
+    fmt.Println("server started at", addr)
+
     // APIs
     // mux.HandleFunc("/", pkg.RootHandler)
-
+    
     // mux.HandleFunc("/api/hello", internal.HelloHandler)
-
+    
     // server dist files for frontend
     mux.HandleFunc("/", internal.ServeFrontend)
+
+    mux.HandleFunc("/api/register-new-user", internal.RegisterUser)
 
     return http.ListenAndServe(addr, mux)
 }
