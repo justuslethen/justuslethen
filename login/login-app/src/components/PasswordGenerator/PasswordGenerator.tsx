578592b94bc5a7c7648737f211191a6d83a311f5
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-08-27 21:26:46 +0200

    add getDifferentCharsCount to passwordGenerator

--- login/login-app/src/components/PasswordGenerator/PasswordGenerator.tsx
+++ login/login-app/src/components/PasswordGenerator/PasswordGenerator.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { t } from '../../i18n.ts';
+import { PASSWORD_LENGTH } from '../../config.ts';
 
 import styles from "./PasswordGenerator.module.css"
 
@@ -13,11 +14,88 @@ interface PasswordGeneratorProps {
 const PasswordGenerator = (props: PasswordGeneratorProps) => {
     const [generatedPassword, changeGeneratedPassword] = useState("Passw-orT")
 
-    // 
+    const createPassword = () => {
+        let password = "";
+
+        const charsCount = getDifferentCharsCount()
+
+
+
+        changeGeneratedPassword(password);
+    }
+
+
+    const getDifferentCharsCount = () => {
+        let charsCount = {
+            numbers: 0,
+            upperChars: 0,
+            lowerChars: 0,
+            specialChars: 0
+        };
+
+        const random: number = getRandomIntCrypto(2, PASSWORD_LENGTH - 2)
+
+        if (random == 2) {
+            charsCount = calcCharsCountV1(charsCount);
+        } else if (random == PASSWORD_LENGTH - 2) {
+            charsCount = calcCharsCountV2(charsCount);
+        } else {
+            charsCount = calcCharsCountV3(charsCount, random);
+        }
+    }
+
+
+    const calcCharsCountV1 = (charsCount: any) => {
+        charsCount.numbers = 1;
+        charsCount.upperChars = 1;
+
+        charsCount.lowerChars = getRandomIntCrypto(1, PASSWORD_LENGTH - 3);
+        charsCount.specialChars = charsCount.numbers + charsCount.upperChars + charsCount.lowerChars - PASSWORD_LENGTH;
+
+        return charsCount;
+    }
+
+
+    const calcCharsCountV2 = (charsCount: any) => {
+        charsCount.lowerChars = 1;
+        charsCount.specialChars = 1;
+
+        charsCount.numbers = getRandomIntCrypto(1, PASSWORD_LENGTH - 3);
+        charsCount.upperChars = charsCount.numbers + charsCount.specialChars + charsCount.lowerChars - PASSWORD_LENGTH;
+
+        return charsCount;
+    }
+
+
+    const calcCharsCountV3 = (charsCount: any, random: number) => {
+        charsCount.lowerChars = getRandomIntCrypto(1, random - 1);
+        charsCount.numbers = random - charsCount.lowerChars;
+
+        charsCount.specialChars = getRandomIntCrypto(1, PASSWORD_LENGTH - random);
+        charsCount.upperChars = PASSWORD_LENGTH - random - charsCount.specialChars;
+
+        return charsCount;
+    }
+
+    
+    const getRandomIntCrypto = (min: number, max: number): number => {
+        // nclude max in min
+        const range = max + 1 - min;
+        if (range <= 0) throw new Error("max must be greater than min");
+
+        // empty array of length 1
+        const randomArray = new Uint32Array(1);
+
+        // fill with random number
+        crypto.getRandomValues(randomArray);
+
+        // calc number with range and add min
+        return min + (randomArray[0] % range);
+    };
+
+
     // 
     props
-    changeGeneratedPassword
-    // 
     // 
 
     return (
--- login/login-app/src/config.ts
+++ login/login-app/src/config.ts
@@ -1,3 +1,4 @@
 // src/config.ts
 export const API_URL = "http://localhost:4300";
 export const APP_NAME = "Account Center";
+export const PASSWORD_LENGTH = 8;
