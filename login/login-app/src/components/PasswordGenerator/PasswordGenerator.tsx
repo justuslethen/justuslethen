c1c5d10cf75a17d4be057dc8dc3c7d6de74f93a8
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-08-27 19:59:25 +0200

    first PasswordGenerator layout

--- login/login-app/src/components/PasswordGenerator/PasswordGenerator.tsx
+++ login/login-app/src/components/PasswordGenerator/PasswordGenerator.tsx
@@ -1,6 +1,41 @@
-const PasswordGenerator = () => {
+import { useState } from "react"
+
+import styles from "./PasswordGenerator.module.css"
+
+import Button from "../Button/Button.tsx"
+import Text from "../Text/Text.tsx"
+
+interface PasswordGeneratorProps {
+    size: string
+}
+
+const PasswordGenerator = (props: PasswordGeneratorProps) => {
+    const [generatedPassword, changeGeneratedPassword] = useState("Passw-orT")
+
+    // 
+    // 
+    props
+    changeGeneratedPassword
+    // 
+    // 
+
     return (
         <>
+            <div className={styles.mainContainer}>
+                <Text text="Header" center={true} type="h2" />
+                <div className={styles.passwordContainer}>
+                    <>
+                        {Array.from(generatedPassword).map(char => {
+                            return (
+                                <div className={styles.charBox}>
+                                    {char}
+                                </div>
+                            )
+                        })}
+                    </>
+                </div>
+                <Button color="blue" text="Button" onclick={() => { }} />
+            </div>
         </>
     )
 }
