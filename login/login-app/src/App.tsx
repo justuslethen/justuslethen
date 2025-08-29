94b0b933c8137619c09746ad6897190c51ff11df
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-08-29 21:56:29 +0200

    add login page to routes

--- login/login-app/src/App.tsx
+++ login/login-app/src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home/Home.tsx';
 import NotFound from './pages/NotFound/NotFound.tsx';
 import Registrate from './pages/Register/Register.tsx';
 import Login from './pages/Login/Login.tsx';
+import LogInCheck from './pages/LogInCheck/LogInCheck.tsx';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Route path="/registrate" element={<Registrate />} />
 
       <Route path="/login" element={<Login />} />
+
+      <Route path="/login-check" element={<LogInCheck />} />
 
       <Route path="*" element={<NotFound />} />
     </Routes>
