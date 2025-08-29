cbe4e6462b9a0e71cb857f0d414a5e47ead56d9a
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-08-29 22:56:33 +0200

    send response to amILoggedIn

--- login/go-backend/internal/handlers.go
+++ login/go-backend/internal/handlers.go
@@ -3,24 +3,27 @@ package internal
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
 
 	"go-backend/pkg"
 )
 
 func SendLogInCheck(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("login-check")
-	
+
 	token, _ := pkg.GetRefreshTokenCockie(w, r)
-
-	fmt.Println("token", token)
-	
 	loggedIn, userid, _ := pkg.CheckRefreshToken(r, token)
-	
-	fmt.Println("loggedIn", loggedIn)
-	fmt.Println("userid", userid)
 
-	if loggedIn {
-		userData := pkg.GetAllUserData(userid)
-		fmt.Println("userData", userData)
+	if !loggedIn {
+		return
 	}
+	userData := pkg.GetAllUserData(userid)
+	fmt.Println("userData", userData)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]any{
+		"data": userData,
+		"loggedIn": true,
+	})
 }
