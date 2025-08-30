4e5099d2109cafa01f02398d92ee14ad0340729f
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-08-30 15:43:34 +0200

    build layout for email verification

--- login/login-app/src/pages/Email/Email.tsx
+++ login/login-app/src/pages/Email/Email.tsx
@@ -1,8 +1,16 @@
 // import styles from "./Email.module.css"
 
+import Text from "../../components/Text/Text.tsx"
+import { t } from "../../i18n.ts"
+import { Outlet } from "react-router-dom"
+
 const Email = () => {
     return (
         <>
+            <div className="text-content">
+                <Text text={t("app_name")} type="h1" />
+            </div>
+            <Outlet />
         </>
     )
 }
--- login/login-app/src/pages/Email/VerifyEmail/VerifyEmail.tsx
+++ login/login-app/src/pages/Email/VerifyEmail/VerifyEmail.tsx
@@ -1,8 +1,35 @@
 // import styles from "./VerifyEmail.module.css"
 
+import Text from "../../../components/Text/Text.tsx"
+import Container from "../../../components/Container/Container.tsx"
+import Input from "../../../components/Input/Input.tsx"
+import Button from "../../../components/Button/Button.tsx"
+import { t } from "../../../i18n.ts"
+
+
 const VerifyEmail = () => {
+    const user = {
+        email: "email"
+    }
+
     return (
         <>
+            <Text text={t("email.verify.title")} type="h2" center={true} />
+
+            <div className="content">
+                <Container maxWidth={380}>
+                    <Text text={`${t("email.verify.text")} ${user.email}`} type="p2" center={true} />
+                    <Input
+                        label={t("input.verification.label")}
+                        placeholder={t("input.verification.placeholder")}
+                    />
+                    <Text text={t("email.verify.button.send-again")} type="h3" center={false} onclick={() => { }} />
+
+
+                    <Button text={t("email.verify.button.skip")} color="grey" onclick={() => { }} />
+                    <Button text={t("email.verify.button.verify")} color="black" onclick={() => { }} />
+                </Container>
+            </div>
         </>
     )
 }
