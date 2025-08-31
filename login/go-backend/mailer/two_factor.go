d3912b353830ec1dead4a80eeacd76e52a7c5875
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-08-31 22:46:21 +0200

    send verification email with html template

--- login/go-backend/mailer/two_factor.go
+++ login/go-backend/mailer/two_factor.go
@@ -1,14 +1,47 @@
 package mailer
 
 import (
-// "fmt"
+    "bytes"
+    "html/template"
+    "log"
+    "go-backend/pkg"
 )
 
+type VerificationEmailData struct {
+	Username 	string
+	Code 		string
+}
+
+
 func SendVerificationEmail(userid int, code string) {
-	email := Email{
-		RecieverAddr:	"test-mail@mail.com",
-		Subject:		"Subject",
-		HtmlBody: 		"Hallo",
-	}
-	SendHTMLEmail(email)
+    username, emailAddress, err := pkg.GetUsernameAndEmail(userid)
+    if err != nil {
+        log.Println("error while loading userdata", err)
+        return
+    }
+
+    data := VerificationEmailData{
+        Username: username,
+        Code:     code,
+    }
+
+    tmpl, err := template.ParseFiles("templates/verification_email.html")
+    if err != nil {
+        log.Println("error while loading templates:", err)
+        return
+    }
+
+    var body bytes.Buffer
+    if err := tmpl.Execute(&body, data); err != nil {
+        log.Println("error while rendering templates:", err)
+        return
+    }
+
+    email := Email{
+        RecieverAddr: emailAddress,
+        Subject:      "Your verification code",
+        HtmlBody:     body.String(),
+    }
+
+    SendHTMLEmail(email)
 }
