427b63935782564bcef8495695ad301cef11341b
justuslethen <138368372+justuslethen@users.noreply.github.com>
2025-09-01 18:10:10 +0200

    merge styles with layout and data

--- login/go-backend/mailer/send.go
+++ login/go-backend/mailer/send.go
@@ -1,16 +1,17 @@
 package mailer
 
 import (
+	"bytes"
 	"go-backend/config"
 	"gopkg.in/gomail.v2"
+	"html/template"
 )
 
 type Email struct {
-	RecieverAddr 	string
-	Subject			string
-	HtmlBody 		string
+	RecieverAddr string
+	Subject      string
+	HtmlBody     string
 }
-
 
 func SendHTMLEmail(email Email) error {
 	c := config.MailerConfig
@@ -25,4 +26,21 @@ func SendHTMLEmail(email Email) error {
 	d := gomail.NewDialer(c.Host, c.Port, c.Username, c.Password)
 
 	return d.DialAndSend(m)
+}
+
+func parseHtmlAndStyle(htmlPath string, data interface{}) (string, error) {
+	tmpl, err := template.ParseFiles(
+		"templates/base.html",
+		"templates/"+htmlPath,
+	)
+	if err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+	if err := tmpl.ExecuteTemplate(&buf, "base.html", data); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
 }
